165118d9fb599f91e7e0d30fe5d4d0bdaf1d13c7
Milton Kabir <drifter.kabir@gmail.com>
2021-07-09 23:09:57 +0600

    Fourth Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -11,13 +11,70 @@ public class Main {
             System.out.println("1. Add matrices\n" +
                     "2. Multiply matrix by a constant\n" +
                     "3. Multiply matrices\n" +
+                    "4. Transpose matrix\n" +
                     "0. Exit");
             System.out.println("Your choice:");
             int ck=sc.nextInt();
             if(ck==0){
                 break;
             }
-            if(ck==1){
+            if(ck==4){
+                System.out.println("1. Main diagonal\n" +
+                        "2. Side diagonal\n" +
+                        "3. Vertical line\n" +
+                        "4. Horizontal line");
+                System.out.println("Your choice:");
+                int ck1=sc.nextInt();
+                int m1,m2,n2,n1;
+                System.out.println("Enter size of matrix:");
+                m1=sc.nextInt();
+                n1= sc.nextInt();
+                System.out.println("Enter matrix:");
+                double [][] ar=new double [m1][n1];
+                for(int i=0;i<m1;i++){
+                    for(int j=0;j<n1;j++){
+                        ar[i][j]=sc.nextDouble();
+                    }
+                }
+                double[][] res=new double[m1][n1];
+                if(ck1==1){
+                    for(int i=0;i<m1;i++){
+                        for(int j=0;j<n1;j++){
+                            res[i][j]=ar[j][i];
+                        }
+                    }
+                }
+                else if(ck1==2){
+                    for(int i=0;i<m1;i++){
+                        for(int j=0;j<n1;j++){
+                            res[i][j]=ar[m1-1-j][n1-1-i];
+                        }
+                    }
+                }
+                else if(ck1==3){
+                    for(int i=0;i<m1;i++){
+                        for(int j=0;j<n1;j++){
+                            res[i][j]=ar[i][n1-1-j];
+                        }
+                    }
+                }
+                else{
+                    for(int i=0;i<m1;i++){
+                        for(int j=0;j<n1;j++){
+                            res[i][j]=ar[m1-1-i][j];
+                        }
+                    }
+                }
+                for(int i=0;i<m1;i++){
+                    for(int j=0;j<n1;j++){
+//                        ar[i][j]+=br[i][j];
+                        System.out.print(res[i][j]+" ");
+                    }
+                    System.out.println();
+                }
+
+            }
+            else if(ck==1){
                 int m1,m2,n2,n1;
                 System.out.println("Enter size of first matrix:");
                 m1=sc.nextInt();
