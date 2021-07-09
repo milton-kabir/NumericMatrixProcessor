0f8b6234375f216c3ec48685eb05742c1657b6e0
Milton Kabir <drifter.kabir@gmail.com>
2021-07-10 00:12:15 +0600

    Finally Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -13,13 +13,41 @@ public class Main {
                     "3. Multiply matrices\n" +
                     "4. Transpose matrix\n" +
                     "5. Calculate a determinant\n" +
+                    "6. Inverse matrix\n" +
                     "0. Exit");
             System.out.println("Your choice:");
             int ck=sc.nextInt();
             if(ck==0){
                 break;
             }
-            if(ck==5){
+            if(ck==6){
+                int m1,m2,n2,n1;
+                System.out.println("Enter size of matrix:");
+                m1=sc.nextInt();
+                n1= sc.nextInt();
+                System.out.println("Enter matrix:");
+                double [][] ar=new double [m1][n1];
+                for(int i=0;i<m1;i++){
+                    for(int j=0;j<n1;j++){
+                        ar[i][j]=sc.nextDouble();
+                    }
+                }
+
+                ar=inverse(ar);
+                if(ar==null){
+                    System.out.println("This matrix doesn't have an inverse.");
+                    continue;
+                }
+                System.out.println("The result is:");
+                for(int i=0;i<m1;i++){
+                    for(int j=0;j<n1;j++){
+//                        ar[i][j]+=br[i][j];
+                        System.out.print(ar[i][j]+" ");
+                    }
+                    System.out.println();
+                }
+            }
+            else if(ck==5){
                 int m1,m2,n2,n1;
                 System.out.println("Enter size of matrix:");
                 m1=sc.nextInt();
@@ -256,4 +284,84 @@ public class Main {
         }
         return (result);
     }
+    private static double[][] inverse(double[][] matrix) {
+        double[][] result;
+        double determinant;
+        double[][] cofactors = new double[matrix.length][matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                cofactors[i][j] = cofactorOf(matrix, i, j);
+            }
+        }
+
+        determinant = matrixDeterminant(matrix);
+        result = determinant == 0 ? null :multipleByConstant(transpose(cofactors, 1), 1 / determinant);
+
+        return result;
+    }
+    private static double cofactorOf(double[][] matrix, int row, int col) {
+        double[][] minor = new double[matrix.length - 1][matrix.length - 1];
+        for (int i = 0; i < minor.length; i++) {
+            for (int j = 0; j < minor.length; j++) {
+                int k = i;
+                int l = j;
+                if (i >= row) {
+                    k++;
+                }
+                if (j >= col) {
+                    l++;
+                }
+                minor[i][j] = matrix[k][l];
+            }
+        }
+        return Math.pow(-1, row + col + 2) * matrixDeterminant(minor);
+    }
+    private static double[][] transpose(double[][] a, int method) {
+        double[][] result = new double[a.length][a[0].length];
+
+        switch (method) {
+            case 1:
+                for (int i = 0; i < result.length; i++) {
+                    for (int j = 0; j < result[0].length; j++) {
+                        result[i][j] = a[j][i];
+                    }
+                }
+                break;
+            case 2:
+                for (int i = 0; i < result.length; i++) {
+                    for (int j = 0; j < result[0].length; j++) {
+                        result[i][j] = a[result.length - 1 - j][result.length - 1 - i];
+                    }
+                }
+                break;
+            case 3:
+                for (int i = 0; i < result.length; i++) {
+                    for (int j = 0; j < result[0].length; j++) {
+                        result[i][j] = a[i][result.length - 1 - j];
+                    }
+                }
+                break;
+            case 4:
+                for (int i = 0; i < result.length; i++) {
+                    for (int j = 0; j < result[0].length; j++) {
+                        result[i][j] = a[result.length - 1 - i][j];
+                    }
+                }
+                break;
+        }
+
+        return result;
+    }
+    private static double[][] multipleByConstant(double[][] matrix, double constant) {
+        double[][] result = matrix.clone();
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = result[i][j] * constant;
+            }
+        }
+
+        return result;
+    }
+
 }
